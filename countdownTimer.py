349390efef2127b9655c59f17fd06925cbe04c39
Rombout Jansen <romboutlambert@gmail.com>
2020-07-24 14:49:57 +0200

    Format all code

--- countdownTimer.py
+++ countdownTimer.py
@@ -9,10 +9,13 @@ import os
 
 subprocess.call("clear")
 
+
 def displayText(text, color):
-    linesProcess = subprocess.Popen("tput lines", stdout=subprocess.PIPE, shell=True)
+    linesProcess = subprocess.Popen(
+        "tput lines", stdout=subprocess.PIPE, shell=True)
     lines = int(linesProcess.stdout.read()[:-1])
-    collumsProcess = subprocess.Popen("tput cols", stdout=subprocess.PIPE, shell=True)
+    collumsProcess = subprocess.Popen(
+        "tput cols", stdout=subprocess.PIPE, shell=True)
     collums = int(collumsProcess.stdout.read()[:-1])
 
     text = text2art(text).split("\n")
@@ -41,6 +44,7 @@ def displayText(text, color):
     if color == "yellow":
         return "\u001b[43;1m" + output + "\u001b[0m"
 
+
 def timeToSeconds(normalTime):
     normalTime = normalTime.split(":")
     timeInMillis = 0
@@ -49,31 +53,36 @@ def timeToSeconds(normalTime):
     timeInMillis += int(normalTime[2])
     return timeInMillis
 
+
 def timer():
-    timer = subprocess.getoutput("termux-dialog -t 'Select time' -i 'Format like h:m:s'")
-    
+    timer = subprocess.getoutput(
+        "termux-dialog -t 'Select time' -i 'Format like h:m:s'")
+
     timer = timeToSeconds(json.loads(timer)["text"])
     endTime = round(time.time()) + timer
-    
+
     while 1:
         time.sleep(0.1)
         timeLeft = endTime-round(time.time())
         if timeLeft > 5:
             color = "green"
         elif timeLeft <= 0:
-            color = "red" 
+            color = "red"
         else:
             color = "yellow"
-        sys.stdout.write(u"\u001b[1000D" + displayText(str(timedelta(seconds=timeLeft)), color)) 
+        sys.stdout.write(
+            u"\u001b[1000D" + displayText(str(timedelta(seconds=timeLeft)), color))
         sys.stdout.flush()
         if color == "red":
             break
     alarm()
 
+
 def alarm():
-    turnOff = subprocess.Popen("termux-dialog confirm -t 'Turn off' -i ''", stdout=subprocess.PIPE, shell=True)
+    turnOff = subprocess.Popen(
+        "termux-dialog confirm -t 'Turn off' -i ''", stdout=subprocess.PIPE, shell=True)
     subprocess.call("termux-notification -t 'Alarm' --button1 'Stop alarm' --button1-action 'echo \"Pressed\" > /data/data/com.termux/files/home/intervalTimer/pressed.txt' --on-delete 'echo \"Pressed\" > /data/data/com.termux/files/home/intervalTimer/pressed.txt' -i 1204", shell=True)
-    
+
     while 1:
         playbeep()
         turnOff.poll()
@@ -82,14 +91,18 @@ def alarm():
                 subprocess.call("termux-notification-remove 1204", shell=True)
                 break
             else:
-                turnOff = subprocess.Popen("termux-dialog confirm -t 'Turn off' -i ''", stdout=subprocess.PIPE, shell=True)
+                turnOff = subprocess.Popen(
+                    "termux-dialog confirm -t 'Turn off' -i ''", stdout=subprocess.PIPE, shell=True)
         try:
             with open("/data/data/com.termux/files/home/intervalTimer/pressed.txt", "r") as f:
                 if f.read() == "Pressed\n":
-                    subprocess.call("termux-notification-remove 1204", shell=True)
-                    os.remove("/data/data/com.termux/files/home/intervalTimer/pressed.txt")
+                    subprocess.call(
+                        "termux-notification-remove 1204", shell=True)
+                    os.remove(
+                        "/data/data/com.termux/files/home/intervalTimer/pressed.txt")
                     break
         except:
             pass
+
 
 timer()
